f6dc06d98aee929ef20077816ca83c2281b40c24
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-09-27 16:44:16 +0200

    LPS-44965 SF: rename method

--- portal-impl/test/integration/com/liferay/portal/upgrade/v7_0_3/UpgradeOracleTest.java
+++ portal-impl/test/integration/com/liferay/portal/upgrade/v7_0_3/UpgradeOracleTest.java
@@ -90,7 +90,7 @@ public class UpgradeOracleTest {
 	public void testUpgradeReduceFieldSizes() throws Exception {
 		_upgradeOracle.upgrade();
 
-		Assert.assertEquals(75, getDataLenght(_TABLE_NAME, _FIELD_NAME));
+		Assert.assertEquals(75, getCharDataLenght(_TABLE_NAME, _FIELD_NAME));
 	}
 
 	@Test
@@ -98,6 +98,25 @@ public class UpgradeOracleTest {
 		_upgradeOracle.upgrade();
 
 		Assert.assertEquals("C", getCharUsed(_TABLE_NAME, _FIELD_NAME));
+	}
+
+	protected int getCharDataLenght(String tableName, String tableColumn)
+		throws Exception {
+
+		try (Connection connection = DataAccess.getUpgradeOptimizedConnection();
+			PreparedStatement ps = connection.prepareStatement(
+				"select data_length from user_tab_columns where " +
+					"table_name = '" + tableName + "' and column_name = '" +
+					tableColumn + "'")) {
+
+			ResultSet rs = ps.executeQuery();
+
+			rs.next();
+
+			int dataLength = rs.getInt(1);
+
+			return dataLength / 4;
+		}
 	}
 
 	protected String getCharUsed(String tableName, String tableColumn)
@@ -114,25 +133,6 @@ public class UpgradeOracleTest {
 			rs.next();
 
 			return rs.getString(1);
-		}
-	}
-
-	protected int getDataLenght(String tableName, String tableColumn)
-		throws Exception {
-
-		try (Connection connection = DataAccess.getUpgradeOptimizedConnection();
-			PreparedStatement ps = connection.prepareStatement(
-				"select data_length from user_tab_columns where " +
-					"table_name = '" + tableName + "' and column_name = '" +
-					tableColumn + "'")) {
-
-			ResultSet rs = ps.executeQuery();
-
-			rs.next();
-
-			int dataLength = rs.getInt(1);
-
-			return dataLength / 4;
 		}
 	}
 

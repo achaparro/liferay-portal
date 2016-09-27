95b94cd6f3eb2e6028a021ebc4771e0205efbf7e
Manuel de la Peña <manuel.delapenya@liferay.com>
2016-09-27 16:21:17 +0200

    LPS-44965 Only update the release for the specific test method

--- portal-impl/test/integration/com/liferay/portal/upgrade/v7_0_3/UpgradeOracleTest.java
+++ portal-impl/test/integration/com/liferay/portal/upgrade/v7_0_3/UpgradeOracleTest.java
@@ -62,12 +62,6 @@ public class UpgradeOracleTest {
 		_db.runSQL(
 			"alter table " + _TABLE_NAME + " modify " + _FIELD_NAME +
 				" varchar2(300 BYTE)");
-
-		Release portalRelease = ReleaseLocalServiceUtil.fetchRelease("portal");
-
-		portalRelease.setBuildNumber(ReleaseInfo.RELEASE_6_1_20_BUILD_NUMBER);
-
-		ReleaseLocalServiceUtil.updateRelease(portalRelease);
 	}
 
 	@After
@@ -88,6 +82,12 @@ public class UpgradeOracleTest {
 
 	@Test
 	public void testUpgradeReduceFieldSizes() throws Exception {
+		Release portalRelease = ReleaseLocalServiceUtil.fetchRelease("portal");
+
+		portalRelease.setBuildNumber(ReleaseInfo.RELEASE_6_1_20_BUILD_NUMBER);
+
+		ReleaseLocalServiceUtil.updateRelease(portalRelease);
+
 		_upgradeOracle.upgrade();
 
 		Assert.assertEquals(75, getCharDataLenght(_TABLE_NAME, _FIELD_NAME));

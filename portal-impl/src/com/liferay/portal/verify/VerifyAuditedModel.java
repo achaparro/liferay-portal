84d19219554250d700dafaf4cc328278d07fc218
Minhchau Dang <minhchau.dang@liferay.com>
2018-03-13 19:21:50 -0700

    LPS-68625 All of the related model updates simply accept the value from the related model, so do this in bulk first

--- portal-impl/src/com/liferay/portal/verify/VerifyAuditedModel.java
+++ portal-impl/src/com/liferay/portal/verify/VerifyAuditedModel.java
@@ -16,6 +16,7 @@ package com.liferay.portal.verify;
 
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
+import com.liferay.portal.kernel.dao.db.DBInspector;
 import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.log.Log;
@@ -261,7 +262,31 @@ public class VerifyAuditedModel extends VerifyProcess {
 		throws Exception {
 
 		try (LoggingTimer loggingTimer = new LoggingTimer(
-				verifiableAuditedModel.getTableName())) {
+				verifiableAuditedModel.getTableName());
+			Connection con = DataAccess.getUpgradeOptimizedConnection()) {
+
+			String relatedModelName =
+				verifiableAuditedModel.getRelatedModelName();
+
+			if (relatedModelName != null) {
+				_bulkVerifyFromRelatedModel(
+					con, verifiableAuditedModel, "userId");
+
+				if (verifiableAuditedModel.isUpdateDates()) {
+					_bulkVerifyFromRelatedModel(
+						con, verifiableAuditedModel, "createDate");
+
+					_bulkVerifyFromRelatedModel(
+						con, verifiableAuditedModel, "modifiedDate");
+				}
+
+				DBInspector dbInspector = new DBInspector(con);
+
+				if (dbInspector.hasColumn(relatedModelName, "userName")) {
+					_bulkVerifyFromRelatedModel(
+						con, verifiableAuditedModel, "userName");
+				}
+			}
 
 			StringBundler sb = new StringBundler(8);
 
@@ -282,8 +307,7 @@ public class VerifyAuditedModel extends VerifyProcess {
 
 			long previousCompanyId = 0;
 
-			try (Connection con = DataAccess.getUpgradeOptimizedConnection();
-				PreparedStatement ps1 = con.prepareStatement(sb.toString());
+			try (PreparedStatement ps1 = con.prepareStatement(sb.toString());
 				ResultSet rs = ps1.executeQuery();
 				PreparedStatement ps2 =
 					AutoBatchPreparedStatementUtil.autoBatch(
@@ -328,6 +352,24 @@ public class VerifyAuditedModel extends VerifyProcess {
 				ps2.executeBatch();
 			}
 		}
+	}
+
+	private void _bulkVerifyFromRelatedModel(
+			Connection con, VerifiableAuditedModel verifiableAuditedModel,
+			String columnName)
+		throws Exception {
+
+		String sql = StringBundler.concat(
+			"update ", verifiableAuditedModel.getTableName(), " set ",
+			columnName, " = (select ", columnName, " from ",
+			verifiableAuditedModel.getRelatedModelName(), " where ",
+			verifiableAuditedModel.getTableName(), StringPool.PERIOD,
+			verifiableAuditedModel.getJoinByTableName(), " = ",
+			verifiableAuditedModel.getRelatedModelName(), StringPool.PERIOD,
+			verifiableAuditedModel.getRelatedPKColumnName(),
+			") where userName is null");
+
+		runSQL(con, sql);
 	}
 
 	private PreparedStatement _createPreparedStatement(

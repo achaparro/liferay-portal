12b39d2ca818f7cccafdcc2c78f78e777607be8f
balazssk <balazs.safrany-kovalik@liferay.com>
2019-01-09 10:23:26 +0100

    LPS-88135 Apply to BaseUpgradeStygingGroupTypeSettings

--- portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseUpgradeStagingGroupTypeSettings.java
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseUpgradeStagingGroupTypeSettings.java
@@ -14,12 +14,6 @@
 
 package com.liferay.portal.kernel.upgrade;
 
-import com.liferay.exportimport.kernel.staging.StagingConstants;
-import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
-import com.liferay.portal.kernel.dao.orm.Property;
-import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
@@ -27,98 +21,53 @@ import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
 
 /**
+ * This class is used to rename a staged portlet in all Group typesettings.
+ *
  * @author Gergely Mathe
  * @author Balázs Sáfrány-Kovalik
  */
-public class BaseUpgradeStagingGroupTypeSettings extends UpgradeProcess {
+public class BaseUpgradeStagingGroupTypeSettings
+	extends BaseUpgradeGroupTypeSettings {
 
 	public BaseUpgradeStagingGroupTypeSettings(
 		CompanyLocalService companyLocalService,
 		GroupLocalService groupLocalService, String oldPortletId,
 		String newPortletId) {
 
-		_companyLocalService = companyLocalService;
-		_groupLocalService = groupLocalService;
+		this.companyLocalService = companyLocalService;
+		this.groupLocalService = groupLocalService;
 		_oldPortletId = oldPortletId;
 		_newPortletId = newPortletId;
 	}
 
-	@Override
-	protected void doUpgrade() throws Exception {
-		updateStagedPortletNames();
-	}
+	protected void updateTypeSettings(
+		UnicodeProperties typeSettingsProperties, Group group) {
 
-	protected void updateStagedPortletNames() throws PortalException {
-		for (Company company : _companyLocalService.getCompanies()) {
-			updateStagedPortletNames(company.getCompanyId());
-		}
-	}
+		String oldPropertyKey = getStagedPortletId(_oldPortletId);
 
-	protected void updateStagedPortletNames(Long companyId)
-		throws PortalException {
+		String oldPropertyValue = typeSettingsProperties.getProperty(
+			oldPropertyKey);
 
-		ActionableDynamicQuery groupActionableDynamicQuery =
-			_groupLocalService.getActionableDynamicQuery();
+		typeSettingsProperties.remove(oldPropertyKey);
 
-		groupActionableDynamicQuery.setAddCriteriaMethod(
-			dynamicQuery -> {
-				Property companyIdProperty = PropertyFactoryUtil.forName(
-					"companyId");
-
-				dynamicQuery.add(companyIdProperty.eq(companyId));
-
-				Property siteProperty = PropertyFactoryUtil.forName("site");
-
-				dynamicQuery.add(siteProperty.eq(Boolean.TRUE));
-			});
-		groupActionableDynamicQuery.setPerformActionMethod(
-			(ActionableDynamicQuery.PerformActionMethod<Group>)group -> {
-				UnicodeProperties typeSettingsProperties =
-					group.getTypeSettingsProperties();
-
-				if (typeSettingsProperties.isEmpty()) {
-					return;
-				}
-
-				String oldPropertyKey = _getStagedPortletId(_oldPortletId);
-
-				String oldPropertyValue = typeSettingsProperties.getProperty(
-					oldPropertyKey);
-
-				typeSettingsProperties.remove(oldPropertyKey);
-
-				if (Validator.isNull(oldPropertyValue)) {
-					return;
-				}
-
-				String newPropertyKey = _getStagedPortletId(_newPortletId);
-
-				String newPropertyValue = typeSettingsProperties.getProperty(
-					newPropertyKey);
-
-				if (Validator.isNull(newPropertyValue)) {
-					typeSettingsProperties.put(
-						newPropertyKey, oldPropertyValue);
-				}
-
-				group.setTypeSettingsProperties(typeSettingsProperties);
-
-				_groupLocalService.updateGroup(group);
-			});
-
-		groupActionableDynamicQuery.performActions();
-	}
-
-	private String _getStagedPortletId(String portletId) {
-		if (portletId.startsWith(StagingConstants.STAGED_PORTLET)) {
-			return portletId;
+		if (Validator.isNull(oldPropertyValue)) {
+			return;
 		}
 
-		return StagingConstants.STAGED_PORTLET.concat(portletId);
+		String newPropertyKey = getStagedPortletId(_newPortletId);
+
+		String newPropertyValue = typeSettingsProperties.getProperty(
+			newPropertyKey);
+
+		if (Validator.isNull(newPropertyValue)) {
+			typeSettingsProperties.put(newPropertyKey, oldPropertyValue);
+		}
+
+		group.setTypeSettingsProperties(typeSettingsProperties);
+
+		groupLocalService.updateGroup(group);
 	}
 
-	private final CompanyLocalService _companyLocalService;
-	private final GroupLocalService _groupLocalService;
 	private final String _newPortletId;
 	private final String _oldPortletId;
 

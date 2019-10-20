5bb52ddb3cb4cd335235c448e0919e9cd5cc8048
Javier Gamarra <nhpatt@gmail.com>
2019-10-20 19:57:19 +0200

    LPS-102810 use new columns

--- portal-impl/src/com/liferay/portlet/ratings/service/impl/RatingsEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/ratings/service/impl/RatingsEntryLocalServiceImpl.java
@@ -29,6 +29,7 @@ import com.liferay.ratings.kernel.model.RatingsEntry;
 import com.liferay.ratings.kernel.model.RatingsStats;
 import com.liferay.social.kernel.model.SocialActivityConstants;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -205,6 +206,7 @@ public class RatingsEntryLocalServiceImpl
 				stats = ratingsStatsLocalService.addStats(classNameId, classPK);
 			}
 
+			stats.setModifiedDate(new Date());
 			stats.setTotalScore(stats.getTotalScore() - oldScore + score);
 			stats.setAverageScore(
 				stats.getTotalScore() / stats.getTotalEntries());
@@ -236,6 +238,7 @@ public class RatingsEntryLocalServiceImpl
 				stats = ratingsStatsLocalService.addStats(classNameId, classPK);
 			}
 
+			stats.setModifiedDate(new Date());
 			stats.setTotalEntries(stats.getTotalEntries() + 1);
 			stats.setTotalScore(stats.getTotalScore() + score);
 			stats.setAverageScore(

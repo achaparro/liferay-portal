ba7bded35f71e8f0dc6ec49283e4b10846585c0f
Alberto Chaparro <alberto.chaparro@liferay.com>
2019-12-05 19:04:13 -0800

    LPS-103035 BuildService Modules

--- modules/apps/depot/depot-service/src/main/resources/META-INF/sql/indexes.sql
+++ modules/apps/depot/depot-service/src/main/resources/META-INF/sql/indexes.sql
@@ -1,6 +1,6 @@
 create unique index IX_884D6226 on DepotEntry (groupId);
 create index IX_FBDFFFF8 on DepotEntry (uuid_[$COLUMN_LENGTH:75$], companyId);
-create unique index IX_8AD8247A on DepotEntry (uuid_[$COLUMN_LENGTH:75$], groupId);
+create unique index IX_418C2F6E on DepotEntry (uuid_[$COLUMN_LENGTH:75$], groupId, companyId);
 
-create unique index IX_65D34444 on DepotEntryGroupRel (depotEntryId, toGroupId);
+create unique index IX_E0E42D64 on DepotEntryGroupRel (depotEntryId, toGroupId, companyId);
 create index IX_DB75E9F1 on DepotEntryGroupRel (toGroupId);

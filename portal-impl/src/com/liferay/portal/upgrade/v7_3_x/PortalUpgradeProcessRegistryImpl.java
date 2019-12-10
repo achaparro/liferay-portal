8f0c52503f3c3ae5220c0900f7401ab4ba013e00
Alberto Chaparro <alberto.chaparro@liferay.com>
2019-12-10 13:26:31 -0800

    LPS-103035 It's a minor because if we come back to previous code will work and if later we install the new version again we don't need to run the upgrade since the not null clause ensure all values are valid

--- portal-impl/src/com/liferay/portal/upgrade/v7_3_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_3_x/PortalUpgradeProcessRegistryImpl.java
@@ -53,7 +53,7 @@ public class PortalUpgradeProcessRegistryImpl
 		upgradeProcesses.put(new Version(8, 0, 0), new UpgradeCompanyInfo());
 
 		upgradeProcesses.put(
-			new Version(9, 0, 0),
+			new Version(8, 1, 0),
 			new UpgradePrimaryKeyCompanyId(
 				"Account_", "Address", "AnnouncementsDelivery",
 				"AnnouncementsEntry", "AnnouncementsFlag", "AssetCategory",

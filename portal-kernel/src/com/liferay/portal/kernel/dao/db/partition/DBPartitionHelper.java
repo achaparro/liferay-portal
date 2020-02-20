fc607a3b2d2fe703d8c0beb5032c350f9f6fef79
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-02-19 17:14:47 -0800

    LPS-108239 Add DB Partition implementation. Company removal will be executed in a separate JVM

---
+++ portal-kernel/src/com/liferay/portal/kernel/dao/db/partition/DBPartitionHelper.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.dao.db.partition;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Alberto Chaparro
+ */
+@ProviderType
+public interface DBPartitionHelper {
+
+	public default void addPartition(long companyId) throws Exception {
+	}
+
+	public default boolean removePartition(long companyId) {
+		return false;
+	}
+
+	public default Connection usePartition(Connection connection)
+		throws SQLException {
+
+		return connection;
+	}
+
+	public default void validate() throws Exception {
+	}
+
+}

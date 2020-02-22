cc7bb719bab917c4705c2905f3a35ea7e5a81237
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-02-21 18:12:04 -0800

    LPS-107396 Implement a more generic logic for DB2

--- portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
+++ portal-impl/src/com/liferay/portal/dao/db/BaseDB.java
@@ -15,8 +15,8 @@
 package com.liferay.portal.dao.db;
 
 import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
-import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.dao.orm.common.SQLTransformer;
 import com.liferay.portal.kernel.configuration.Filter;
@@ -131,7 +131,8 @@ public abstract class BaseDB implements DB {
 	}
 
 	@Override
-	public abstract String buildSQL(String template) throws IOException;
+	public abstract String buildSQL(String template)
+		throws IOException, SQLException;
 
 	/**
 	 * @deprecated As of Athanasius (7.3.x), with no direct replacement
@@ -329,10 +330,6 @@ public abstract class BaseDB implements DB {
 
 				if (sql.endsWith("\n/")) {
 					sql = sql.substring(0, sql.length() - 2);
-				}
-
-				if (_log.isDebugEnabled()) {
-					_log.debug(sql);
 				}
 
 				try {
@@ -812,7 +809,8 @@ public abstract class BaseDB implements DB {
 		return _applyMaxStringIndexLengthLimitation(sb.toString());
 	}
 
-	protected abstract String reword(String data) throws IOException;
+	protected abstract String reword(String data)
+		throws IOException, SQLException;
 
 	protected static final String ALTER_COLUMN_NAME = "alter_column_name ";
 

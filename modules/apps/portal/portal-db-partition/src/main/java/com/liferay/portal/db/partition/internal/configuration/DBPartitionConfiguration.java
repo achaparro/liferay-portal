c751fce4214f681bc3cc43a91f309b5219750248
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-04-29 16:36:08 -0700

    LPS-108588 DB Partition Destination interceptor and configuration implementation

---
+++ modules/apps/portal/portal-db-partition/src/main/java/com/liferay/portal/db/partition/internal/configuration/DBPartitionConfiguration.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.db.partition.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Alberto Chaparro
+ */
+@ExtendedObjectClassDefinition(category = "infrastructure")
+@Meta.OCD(
+	id = "com.liferay.portal.db.partition.internal.configuration.DBPartitionConfiguration",
+	localization = "content/Language", name = "db-partition-configuration-name"
+)
+public interface DBPartitionConfiguration {
+
+	@Meta.AD(
+		description = "excluded-message-bus-destination-names-description",
+		name = "excluded-message-bus-destination-names", required = false
+	)
+	public String[] excludedMessageBusDestinationNames();
+
+	@Meta.AD(
+		description = "excluded-scheduler-job-names-description",
+		name = "excluded-scheduler-job-names", required = false
+	)
+	public String[] excludedSchedulerJobNames();
+
+}

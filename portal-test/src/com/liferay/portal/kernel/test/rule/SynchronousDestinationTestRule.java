2f6873c5e60f582a27fdddd4cfd978616048e2a9
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-05-26 16:31:51 -0700

    LPS-10588 When we complete the transaction at this point, the entitty cache can be out of date when objects have been modified several times and the mvccVersion increased

--- portal-test/src/com/liferay/portal/kernel/test/rule/SynchronousDestinationTestRule.java
+++ portal-test/src/com/liferay/portal/kernel/test/rule/SynchronousDestinationTestRule.java
@@ -17,6 +17,7 @@ package com.liferay.portal.kernel.test.rule;
 import com.liferay.petra.lang.SafeClosable;
 import com.liferay.petra.reflect.ReflectionUtil;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.orm.EntityCacheUtil;
 import com.liferay.portal.kernel.dependency.manager.DependencyManagerSyncUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
@@ -415,7 +416,7 @@ public class SynchronousDestinationTestRule
 			try {
 				TransactionInvokerUtil.invoke(
 					_transactionConfig,
-					new Callable<Void>() {
+					new Callable<Void>()
 
 						@Override
 						public Void call() throws Exception {
@@ -426,6 +427,8 @@ public class SynchronousDestinationTestRule
 						}
 
 					});
+
+				EntityCacheUtil.clearCache();
 			}
 			catch (Throwable t) {
 				throw new RuntimeException(t);

b41ff62b16028276bc4f6d17bc1a161d984726ec
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-07-19 19:14:43 -0700

    LPS-117403 Add javadoc since it can be confusing

--- portal-impl/src/com/liferay/portal/events/StartupHelperUtil.java
+++ portal-impl/src/com/liferay/portal/events/StartupHelperUtil.java
@@ -90,6 +90,12 @@ public class StartupHelperUtil {
 		return !_portalUpgrading;
 	}
 
+	/**
+	 * Returns <code>true</code> if the portal or the modules are upgrading.
+	 *
+	 * @return <code>true</code> if the portal or the modules are upgrading;
+	 * <code>false</code> otherwise
+	 */
 	public static boolean isUpgrading() {
 		return _upgrading;
 	}

7decd05af43d2e1a75b736788a52c8693a00b6ba
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-10-25 20:35:10 +0100

    LPS-108590 Apply changes

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
@@ -205,7 +205,7 @@ public class CompanyIndexFactory
 	}
 
 	protected synchronized void createCompanyIndexes() {
-		CompaniesUtil.runCompanyIds(
+		CompaniesUtil.forEachCompanyId(
 			companyId -> {
 				RestHighLevelClient restHighLevelClient =
 					_elasticsearchConnectionManager.getRestHighLevelClient();

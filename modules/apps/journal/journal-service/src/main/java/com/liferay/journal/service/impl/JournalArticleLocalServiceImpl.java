168b7572c1af6028086be6eefb13ecb4b5dac676
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-10-25 17:49:54 +0100

    LPS-108590 We should always get companies without the system

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -7185,8 +7185,7 @@ public class JournalArticleLocalServiceImpl
 
 		CompaniesUtil.run(
 			company -> checkArticlesByCompanyIdAndExpirationDate(
-				company.getCompanyId(), expirationDate, nextExpirationDate),
-			companyLocalService.getCompanies(false));
+				company.getCompanyId(), expirationDate, nextExpirationDate));
 
 		if (_previousCheckDate == null) {
 			_previousCheckDate = new Date(

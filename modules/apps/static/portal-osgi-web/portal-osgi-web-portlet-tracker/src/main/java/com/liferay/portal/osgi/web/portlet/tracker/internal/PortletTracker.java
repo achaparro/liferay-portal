e914ba0800312bd76f9fc00e9e932c0bf4f766c8
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-10-25 20:35:10 +0100

    LPS-108590 Apply changes

--- modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-portlet-tracker/src/main/java/com/liferay/portal/osgi/web/portlet/tracker/internal/PortletTracker.java
@@ -229,7 +229,7 @@ public class PortletTracker
 
 		_portletInstanceFactory.destroy(portletModel);
 
-		CompaniesUtil.runCompanyIds(
+		CompaniesUtil.forEachCompanyId(
 			companyId -> {
 				PortletCategory portletCategory =
 					(PortletCategory)WebAppPool.get(

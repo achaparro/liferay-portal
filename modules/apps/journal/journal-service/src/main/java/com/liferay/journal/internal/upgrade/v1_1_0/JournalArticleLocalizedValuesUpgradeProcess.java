a8edec835649fc97578fc85c77852d09239af3ea
Preston-Crary <preston.crary@liferay.com>
2021-08-11 15:34:31 -0700

    LPS-137225 Lock CompanyThreadLocal around each task.

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v1_1_0/JournalArticleLocalizedValuesUpgradeProcess.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v1_1_0/JournalArticleLocalizedValuesUpgradeProcess.java
@@ -135,7 +135,7 @@ public class JournalArticleLocalizedValuesUpgradeProcess
 
 					return null;
 				},
-				BaseUpgradeCallable::call);
+				UpdateJournalArticleLocalizedFieldsUpgradeCallable::doCall);
 		}
 	}
 

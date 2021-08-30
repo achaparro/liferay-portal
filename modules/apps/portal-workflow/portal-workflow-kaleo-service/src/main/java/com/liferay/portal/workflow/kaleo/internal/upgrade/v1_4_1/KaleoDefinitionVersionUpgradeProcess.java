39dd855f0f2cfde5dde5173178d5e962d5fe9ce3
Alberto Chaparro <alberto.chaparro@liferay.com>
2021-08-30 13:51:53 +0200

    LPS-134196 Apply the new API

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/upgrade/v1_4_1/KaleoDefinitionVersionUpgradeProcess.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/upgrade/v1_4_1/KaleoDefinitionVersionUpgradeProcess.java
@@ -21,7 +21,6 @@ import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
-import com.liferay.portal.workflow.kaleo.internal.upgrade.v1_4_1.util.KaleoDefinitionTable;
 
 import java.io.IOException;
 
@@ -93,9 +92,7 @@ public class KaleoDefinitionVersionUpgradeProcess extends UpgradeProcess {
 
 	protected void removeStartKaleoNodeId() throws Exception {
 		if (hasColumn("KaleoDefinition", "startKaleoNodeId")) {
-			alter(
-				KaleoDefinitionTable.class,
-				new AlterTableDropColumn("startKaleoNodeId"));
+			alterTableDropColumn("KaleoDefinition", "startKaleoNodeId");
 		}
 	}
 

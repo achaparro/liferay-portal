9b4e407b6b5486549b5d97fa37e05b8bc42aea8d
Alberto Chaparro <alberto.chaparro@liferay.com>
2021-09-08 17:18:05 +0200

    LPS-134196 These cases need to check the column first now. We will remove the need of this check before an alter in another LPS

--- modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/internal/upgrade/v1_2_0/StyleBookEntryVersionUpgradeProcess.java
+++ modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/internal/upgrade/v1_2_0/StyleBookEntryVersionUpgradeProcess.java
@@ -33,10 +33,15 @@ public class StyleBookEntryVersionUpgradeProcess extends UpgradeProcess {
 	}
 
 	protected void upgradeSchema() throws Exception {
-		alterTableAddColumn(
-			"StyleBookEntryVersion", "modifiedDate", "DATE null");
-		alterTableAddColumn(
-			"StyleBookEntryVersion", "uuid_", "VARCHAR(75) null");
+		if (!hasColumn("StyleBookEntryVersion", "modifiedDate")) {
+			alterTableAddColumn(
+				"StyleBookEntryVersion", "modifiedDate", "DATE null");
+		}
+
+		if (!hasColumn("StyleBookEntryVersion", "uuid_")) {
+			alterTableAddColumn(
+				"StyleBookEntryVersion", "uuid_", "VARCHAR(75) null");
+		}
 
 		try (LoggingTimer loggingTimer = new LoggingTimer()) {
 			try (PreparedStatement preparedStatement1 =

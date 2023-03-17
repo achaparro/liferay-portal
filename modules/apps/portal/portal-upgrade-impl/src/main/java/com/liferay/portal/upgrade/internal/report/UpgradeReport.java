7c176b743c1ca6b4e30ef249c3ae267c15a01ed4
Luis Ortiz <luis.ortiz@liferay.com>
2023-03-17 11:54:54 +0100

    LPS-171387 Better naming

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/report/UpgradeReport.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/report/UpgradeReport.java
@@ -229,9 +229,9 @@ public class UpgradeReport {
 				}
 			).build()
 		).put(
-			"type", DBUpgradeStatus.getUpgradeType()
+			"type", DBUpgradeStatus.getType()
 		).put(
-			"result", DBUpgradeStatus.getUpgradeStatus()
+			"result", DBUpgradeStatus.getStatus()
 		).put(
 			"database.version",
 			() -> {

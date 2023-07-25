020dfbf695044c02bf425aa926a7d39f45a7c400
Alberto Chaparro <alberto.chaparro@liferay.com>
2023-07-25 09:58:56 +0200

    LPS-190855 Add unit tests

--- portal-impl/test/unit/com/liferay/portal/dao/db/HypersonicDBTest.java
+++ portal-impl/test/unit/com/liferay/portal/dao/db/HypersonicDBTest.java
@@ -42,6 +42,13 @@ public class HypersonicDBTest extends BaseDBTestCase {
 	}
 
 	@Test
+	public void testRewordAlterColumnTypeBigDecimal() throws Exception {
+		Assert.assertEquals(
+			"alter table DLFolder alter column userId decimal(30, 16);\n",
+			buildSQL("alter_column_type DLFolder userId BIGDECIMAL;"));
+	}
+
+	@Test
 	public void testRewordAlterColumnTypeNoSemicolon() throws Exception {
 		Assert.assertEquals(
 			"alter table DLFolder alter column userName varchar(75);\n",

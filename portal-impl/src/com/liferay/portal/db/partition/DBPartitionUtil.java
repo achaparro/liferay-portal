9f4b706c697cd1ed852e4e67b1093d6c3d6e98a0
Luis Ortiz <luis.ortiz@liferay.com>
2023-10-11 18:25:54 +0200

    LPS-194500 Simplifying logic to know if a table is a control table or not

--- portal-impl/src/com/liferay/portal/db/partition/DBPartitionUtil.java
+++ portal-impl/src/com/liferay/portal/db/partition/DBPartitionUtil.java
@@ -90,9 +90,7 @@ public class DBPartitionUtil {
 						continue;
 					}
 
-					if (dbInspector.isControlTable(
-							_getCompanyIds(), tableName)) {
-
+					if (dbInspector.isControlTable(tableName)) {
 						statement.executeUpdate(
 							_getCreateViewSQL(companyId, tableName));
 					}
@@ -297,8 +295,7 @@ public class DBPartitionUtil {
 				while (resultSet.next()) {
 					String tableName = resultSet.getString("TABLE_NAME");
 
-					if (dbInspector.isControlTable(
-							_getCompanyIds(), tableName) &&
+					if (dbInspector.isControlTable(tableName) &&
 						dbInspector.hasColumn(tableName, "companyId")) {
 
 						statement.executeUpdate(
@@ -594,7 +591,7 @@ public class DBPartitionUtil {
 		try {
 			DBInspector dbInspector = new DBInspector(connection);
 
-			if (dbInspector.isControlTable(_getCompanyIds(), tableName) &&
+			if (dbInspector.isControlTable(tableName) &&
 				!(CompanyThreadLocal.getCompanyId() == _defaultCompanyId)) {
 
 				return true;
@@ -631,9 +628,7 @@ public class DBPartitionUtil {
 				while (resultSet.next()) {
 					String tableName = resultSet.getString("TABLE_NAME");
 
-					if (dbInspector.isControlTable(
-							_getCompanyIds(), tableName)) {
-
+					if (dbInspector.isControlTable(tableName)) {
 						controlTableNames.add(tableName);
 
 						_migrateTable(
@@ -758,9 +753,7 @@ public class DBPartitionUtil {
 					DBInspector dbInspector = new DBInspector(connection);
 					String tableName = query[2];
 
-					if (!dbInspector.isControlTable(
-							_getCompanyIds(), tableName)) {
-
+					if (!dbInspector.isControlTable(tableName)) {
 						return returnValue;
 					}
 

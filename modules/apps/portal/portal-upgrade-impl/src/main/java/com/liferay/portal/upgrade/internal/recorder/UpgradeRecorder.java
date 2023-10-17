c43bf47e712ed10e1d8345c61ae3757617c3f5fb
Alberto Chaparro <alberto.chaparro@liferay.com>
2023-10-17 18:27:48 +0200

    LPS-197611 When the module did not exist, the initial schema version is 0.0.0

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/recorder/UpgradeRecorder.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/recorder/UpgradeRecorder.java
@@ -226,10 +226,6 @@ public class UpgradeRecorder {
 
 			SchemaVersions schemaVersions = schemaVersionsEntry.getValue();
 
-			if (schemaVersions._getInitial() == null) {
-				continue;
-			}
-
 			Version initialVersion = Version.parseVersion(
 				schemaVersions._getInitial());
 			Version finalVersion = Version.parseVersion(
@@ -293,7 +289,7 @@ public class UpgradeRecorder {
 					servletContextName);
 
 				if (moduleSchemaVersions == null) {
-					moduleSchemaVersions = new SchemaVersions(null);
+					moduleSchemaVersions = new SchemaVersions();
 
 					_schemaVersionsMap.put(
 						servletContextName, moduleSchemaVersions);
@@ -346,10 +342,6 @@ public class UpgradeRecorder {
 
 	private class SchemaVersions {
 
-		public SchemaVersions(String initial) {
-			_initial = initial;
-		}
-
 		private String _getFinal() {
 			return _final;
 		}
@@ -364,8 +356,6 @@ public class UpgradeRecorder {
 
 		private void _setInitial(String initial) {
 			if (initial == null) {
-				_initial = "0.0.0";
-
 				return;
 			}
 
@@ -373,7 +363,7 @@ public class UpgradeRecorder {
 		}
 
 		private String _final;
-		private String _initial;
+		private String _initial = "0.0.0";
 
 	}
 

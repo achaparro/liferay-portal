89eb4e143db9b7f6acc577d87adbc99ae67a3fa6
Alberto Chaparro <alberto.chaparro@liferay.com>
2023-11-18 00:24:21 +0100

    LPS-197686 New modules with database changes  are major upgrades

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/recorder/UpgradeRecorder.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/recorder/UpgradeRecorder.java
@@ -218,7 +218,7 @@ public class UpgradeRecorder {
 			SchemaVersions schemaVersions = schemaVersionsEntry.getValue();
 
 			if (schemaVersions._getInitial() == null) {
-				continue;
+				return "major";
 			}
 
 			Version initialVersion = Version.parseVersion(

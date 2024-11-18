3cfe0d8ba231fe73fb116b14c15e48587500b356
Alberto Chaparro <alberto.chaparro@liferay.com>
2024-11-18 12:46:32 +0100

    LPD-41266 Missing unnecessary removal

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceDBPartitionTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceDBPartitionTest.java
@@ -674,49 +674,44 @@ public class CompanyLocalServiceDBPartitionTest
 
 	@Test
 	public void testPrefsPropsImplCache() throws Exception {
-		try {
-			try (SafeCloseable safeCloseable =
-					CompanyThreadLocal.setCompanyIdWithSafeCloseable(
-						TestPropsValues.getCompanyId())) {
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(
+					TestPropsValues.getCompanyId())) {
 
-				_portalPreferencesLocalService.updatePreferences(
-					TestPropsValues.getCompanyId(),
-					PortletKeys.PREFS_OWNER_TYPE_COMPANY,
-					"<portlet-preferences><preference><name>testName</name>" +
-						"<value>testValue</value></preference>" +
-							"</portlet-preferences>");
-			}
-
-			EntityCacheUtil.clearCache(CompanyImpl.class);
-
-			Map<Long, PortletPreferences> portletPreferencesMap =
-				(Map<Long, PortletPreferences>)ReflectionTestUtil.getFieldValue(
-					PrefsPropsUtil.getPrefsProps(), "_portletPreferences");
-
-			portletPreferencesMap.clear();
-
-			try (SafeCloseable safeCloseable =
-					CompanyThreadLocal.setCompanyIdWithSafeCloseable(0L)) {
-
-				companyLocalService.forEachCompanyId(
-					companyId -> {
-						if (companyId != TestPropsValues.getCompanyId()) {
-							return;
-						}
-
-						PortletPreferences portletPreferences =
-							portletPreferencesMap.get(
-								TestPropsValues.getCompanyId());
-
-						Assert.assertEquals(
-							"testValue",
-							portletPreferences.getValue("testName", null));
-					},
-					PortalInstancePool.getCompanyIds());
-			}
+			_portalPreferencesLocalService.updatePreferences(
+				TestPropsValues.getCompanyId(),
+				PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+				"<portlet-preferences><preference><name>testName</name>" +
+					"<value>testValue</value></preference>" +
+						"</portlet-preferences>");
 		}
-		finally {
-			companyLocalService.deleteCompany(TestPropsValues.getCompanyId());
+
+		EntityCacheUtil.clearCache(CompanyImpl.class);
+
+		Map<Long, PortletPreferences> portletPreferencesMap =
+			(Map<Long, PortletPreferences>)ReflectionTestUtil.getFieldValue(
+				PrefsPropsUtil.getPrefsProps(), "_portletPreferences");
+
+		portletPreferencesMap.clear();
+
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(0L)) {
+
+			companyLocalService.forEachCompanyId(
+				companyId -> {
+					if (companyId != TestPropsValues.getCompanyId()) {
+						return;
+					}
+
+					PortletPreferences portletPreferences =
+						portletPreferencesMap.get(
+							TestPropsValues.getCompanyId());
+
+					Assert.assertEquals(
+						"testValue",
+						portletPreferences.getValue("testName", null));
+				},
+				PortalInstancePool.getCompanyIds());
 		}
 	}
 

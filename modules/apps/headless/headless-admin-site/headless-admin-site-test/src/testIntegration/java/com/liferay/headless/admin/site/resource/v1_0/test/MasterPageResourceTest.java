827f99f1f8be0cf14da1abeb7b23102dd55599cd
Alberto Chaparro <alberto.chaparro@liferay.com>
2025-11-14 00:51:24 +0100

    LPD-58937 Sort

--- modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/MasterPageResourceTest.java
+++ modules/apps/headless/headless-admin-site/headless-admin-site-test/src/testIntegration/java/com/liferay/headless/admin/site/resource/v1_0/test/MasterPageResourceTest.java
@@ -183,8 +183,8 @@ public class MasterPageResourceTest extends BaseMasterPageResourceTestCase {
 
 		for (MasterPage masterPage : page.getItems()) {
 			if (StringUtil.equals(
-					postMasterPage.getExternalReferenceCode(),
-					masterPage.getExternalReferenceCode())) {
+					masterPage.getExternalReferenceCode(),
+					postMasterPage.getExternalReferenceCode())) {
 
 				URLReference thumbnail = masterPage.getThumbnail();
 
